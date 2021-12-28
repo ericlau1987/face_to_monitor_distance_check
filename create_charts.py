ee9ed839dd5218be5b907dafe5df5af6e5cf7a45
ericlau1987 <ericlau1987@gmail.com>
2021-12-28 23:32:26 +1100

    add indicators

--- create_charts.py
+++ create_charts.py
@@ -9,6 +9,7 @@ import plotly.graph_objs as go
 import pandas as pd
 from sqlalchemy import create_engine
 import pymysql
+import numpy as np
 
 def data_processing(input: pd.DataFrame) -> pd.DataFrame:
 
@@ -29,6 +30,7 @@ df = pd.read_sql_query(
     """
     select *
     from distance
+    where date(date_time) >= '2021-12-25'
     """
 ,dbConnection)
 
@@ -40,6 +42,40 @@ x = df_latest_date['date_time'].to_list()
 y = df_latest_date['distances'].to_list()
 y_distance_limit = df_latest_date['distance_limit'].to_list()
 
+# Indicator for latest date
+date_range = list(df['date'].unique())
+date_range = sorted(date_range)
+prior_date = date_range[-2] 
+avg_distance_latest_date = np.mean(df_latest_date['distances'])
+avg_distance_prior_date = np.mean(df[df['date'] == prior_date]['distances'])
+
+fig_indicator = go.Figure()
+fig_indicator.add_trace(go.Indicator(
+    value = np.mean(avg_distance_latest_date),
+    delta = {'reference': 50},
+    gauge = {
+        'axis': {'visible': False}},
+    title = {'text': "Latest date: Average Distances vs Threshold (CM)"},
+    domain = {'row': 0, 'column': 0}))
+
+
+fig_indicator.add_trace(go.Indicator(
+    mode = "number+delta",
+    value = avg_distance_latest_date,
+    delta = {'reference': avg_distance_prior_date},
+    title = {'text': "Average Distances (latest vs prior date)"},
+    domain = {'row': 0, 'column': 1}))
+
+
+fig_indicator.update_layout(
+    grid = {'rows': 1, 'columns': 2, 'pattern': "independent"},
+    template = {'data' : {'indicator': [{
+        'mode' : "number+delta+gauge",
+        'delta' : {'reference': 90}}]
+                         }})
+
+
+# Distance over time at the latest date
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=x, y=y,
             mode='lines',
@@ -47,20 +83,55 @@ fig.add_trace(go.Scatter(x=x, y=y,
 ))
 fig.add_trace(go.Scatter(x=x, y=y_distance_limit,
             mode='lines',
-            name='distance limits'
+            name='distance threshold'
 ))
 
+fig.update_layout(
+    title="Distances over time (Latest date)",
+    xaxis_title = "Date time",
+    yaxis_title = "Distances"
+
+)
+
+# Distance over time for all time
+fig_distance_all = go.Figure()
+fig_distance_all.add_trace(go.Scatter(x=df['date_time'].to_list(), y=df['distances'].to_list(),
+            mode='lines',
+            name='distances'
+))
+fig_distance_all.add_trace(go.Scatter(x=df['date_time'].to_list(), y=df['distance_limit'].to_list(),
+            mode='lines',
+            name='distance threshold'
+))
+
+fig_distance_all.update_layout(
+    title="Distances over time (All data)",
+    xaxis_title = "Date time",
+    yaxis_title = "Distances"
+
+)
+
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Hello Eric'),
 
     html.Div(children='''
-        Dash: A web application for distances data.
+        Jeffrey's distance to monitors report.
     '''),
 
     dcc.Graph(
+        id='indicator-avg-distance',
+        figure=fig_indicator
+
+    ),
+    dcc.Graph(
         id='example-graph',
         figure=fig
+    ),
+    dcc.Graph(
+        id='graph1',
+        figure=fig_distance_all
     )
+
 ])
 
 if __name__ == '__main__':
